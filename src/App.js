663260143537e9af76d00422ea9114d73a4ec9d9
Andre Santiago Correia <andrecorreia0407@gmail.com>
2025-02-13 16:54:13 -0500

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Use BrowserRouter
 import Navbar from './Navbar/Navbar';  // Import the Navbar component
 import About from './About/About';    // Import the About component
 import Projects from './Projects/Projects';  // Import the Projects component
@@ -10,15 +11,22 @@ import Profile from './Profile/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar /> {/* Add the Navbar at the top */}
-      
-      <div className="main-content">
-      <Profile />
-      </div>
+    <Router>
+      <div className="App">
+        <Navbar /> {/* Add the Navbar at the top */}
+        
+        <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/projects" element={<Projects/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+        </div>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,17 +1,18 @@
 // src/Navbar.js
 import React from 'react';
 import './Navbar.css'; // We'll add styles for the navbar here
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <div className="logo">My Portfolio</div>
+      <div className="logo"><Link to="/">My Portfolio</Link></div>
         <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </div>
     </nav>
